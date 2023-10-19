c4487a6555ea487b9e75ded7ecb2f578cca9001e
Gal Moore <gal@galmoore.co.uk>
2023-10-19 14:37:12 +0100

    Update app.py

--- app.py
+++ app.py
@@ -12,4 +12,17 @@ user_api_key = st.sidebar.text_input(
     placeholder="Paste your openAI API key, sk-",
     type="password")
 
+# Set the OpenAI API key from Streamlit secrets
+# openai.api_key = st.secrets["openai_password"]
+user_api_key = st.secrets["openai_password"]
 uploaded_file = st.sidebar.file_uploader("upload", type="csv")
+
+if uploaded_file :
+   #use tempfile because CSVLoader only accepts a file_path
+    with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
+        tmp_file.write(uploaded_file.getvalue())
+        tmp_file_path = tmp_file.name
+
+    loader = CSVLoader(file_path=tmp_file_path, encoding="utf-8", csv_args={
+                'delimiter': ','})
+    data = loader.load()
